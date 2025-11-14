076269451d1184dd454ad2189cf1be924afffda0
dripska <mitusin242@gmail.com>
2025-11-14 22:57:15 +0300

    Update 1step.py

--- 1step.py
+++ 1step.py
@@ -5,3 +5,6 @@ def get_move(player, board):
         except ValueError:
             print("Введите число от 1 до 9.")
             continue
+        if not 1 <= pos <= 9:
+            print("Позиция вне диапазона. Попробуйте снова.")
+            continue
